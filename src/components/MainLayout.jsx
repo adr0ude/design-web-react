36aa09ded47e70f4b3db2d5b731198c597f93c61
Dhiego Cavalcanti da Silveira <dhiegocsilveira@gmail.com>
2025-08-08 16:07:20 -0300

    Update MainLayout.jsx

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import ScrollToTop from './ScrollToTop';
 
 export default function MainLayout () {
   return (
     <>
       <Header />
       <Outlet />
-      <ScrollToTop />
     </>
   );
 };
